ef9533e0901529ea8ac50d5c9ece833ae4e8326e
edenaii <rsgrenovation@gmail.com>
2025-02-11 12:34:34 +0000

    Code generated by CodeCraftAI at 2025-02-11 12:34:17

---
+++ app/AppName/app/src/main/java/com/example/appname/di/AppModule.kt
@@ -0,0 +1,25 @@
+package com.example.appname.di
+
+import com.example.appname.services.CoinGeckoService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideCoinGeckoService(): Coin极客时间GeckoService {
+        return Retrofit.Builder()
+            .baseUrl("https://api.coingecko.com/api/v3/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CoinGeckoService::class.java)
+    }
+}
